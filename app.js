e3144ae9276de3522b044bee2d12aeab69d41ecf
ENZO FELIX CARRILHO <25132408@CFP127ALUNO.LOCAL>
2025-08-12 11:11:24 -0300

    Projeto Finalizado

--- app.js
+++ app.js
@@ -3,3 +3,27 @@
 import funcionarios from "./funcionarios.json" with {type: "json"}
 
 const container = document.getElementById('container')
+
+function criarCard(funcionario){
+    const div = document.createElement('div')
+    const imagem = document.createElement('img')
+    const nome = document.createElement('h2')
+    const cargo = document.createElement('p')
+
+    imagem.src = `./img/${funcionario.imagem}`
+    nome.textContent = funcionario.nome
+    cargo.textContent = funcionario.cargo
+
+    container.appendChild(div)
+    div.appendChild(imagem)
+    div.append(nome)
+    div.append(cargo)
+
+    
+}
+
+function carregarCards(){
+    funcionarios.forEach(criarCard)
+}
+
+carregarCards()
